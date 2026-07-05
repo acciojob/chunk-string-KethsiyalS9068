696cf1caa03e0f41e24fe85d9dca8164184d1297
KethsiyalS9068 <kethsiyalmail@gmail.com>
2026-07-05 15:04:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function stringChop(str, size) {
 }
 	
 }
-
+chunkString("Hello, world!",5)
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
